0ed233340840ee1dd3c4f75db1e07065e6cc5524
kaifulam <kaifulam@u.washington.edu>
2016-12-16 06:44:55 -0800

    Create Test-ML_Hbb_feature_plot_rev3.py
    To plot histograms using csv files generated by Test-ML_Hbb_feature_histo_rev3.py

---
+++ Var_histo_plots/Test-ML_Hbb_feature_plot_rev3.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+Histo_Bg = np.genfromtxt("Histo_Bg.csv", delimiter=',')
+Bino_Bg = np.genfromtxt("Bins_Bg.csv", delimiter=',')
+Histo_Sig = np.genfromtxt("Histo_Sig.csv", delimiter=',')
+Bino_Sig = np.genfromtxt("Bins_Sig.csv", delimiter=',')
+Features = ["pt", "eta", 'dphi', 'energy', 'weight']
+
+fea_num = Histo_Sig.shape[1]
+
+for i in range(fea_num):
+    center = (Bino_Sig[:-1,i] + Bino_Sig[1:,i])/2
+    width = 0.7 * (Bino_Sig[1,i] - Bino_Sig[0,i])
+    plt.figure(i)
+    plt.step(Bino_Bg[:-1,i], Histo_Bg[:,i], color='red', label='bg')
+    plt.step(Bino_Sig[:-1,i], Histo_Sig[:,i], color='blue', label='sig')
+    #plt.bar(Bino_Sig[:-1,i], Histo_Sig[:,i], color='blue', label='sig', width = 0.1)
+    #plt.bar(center, Histo_Sig[:,i], align='center',width=width)
+    plt.legend()
+    plt.suptitle(Features[i])
+    plt.xlim(-1,1)
+    plt.savefig(Features[i]+'.png')
+    #plt.show(block=True)pt
+    plt.close
